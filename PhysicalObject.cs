c0e8d5573f85e2664f096fedafb586e4b85f88fa
Aaron Priestley <apriestley7@gmail.com>
2015-10-20 19:46:02 +1100

    Changed damage modifier again for land collisions. Thinking 10 is a bit too low.

--- PhysicalObject.cs
+++ PhysicalObject.cs
@@ -171,7 +171,7 @@ namespace Project
 			// Land coliision handling
 			if (game.worldBase.isColiding(pos + (velocity * time), myModel.collisionRadius)){
 				// deal slight damage for hitting edge, just change the multiply factor to adjust
-				float damage = velocity.Length() * 10;
+				float damage = velocity.Length() * 50;
 				hitpoints -= (int)damage;
 				// adjust physics appropriately
 				pos = pos -Vector3.Normalize(velocity) * (myModel.collisionRadius + 0.001f);
